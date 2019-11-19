edb8f4cdcc23d1ea7901b8ba04a1f8236cee6087
keskin <keskin.mustafa.42@gmail.com>
2019-11-19 14:01:56 +0100

    Format indentation

--- database.py
+++ database.py
@@ -4,35 +4,35 @@
 
 class Database(object):
 
-	def __init__(self, core):
-		self.graph = {core: []}
-		self.images_nodes = {}
-		self.images_status = {}
-		self.status_choices = {'invalid': 'invalid', 'valid': 'valid', 'granularity_staged': 'granularity_staged', 'coverage_staged': 'coverage_staged'}
-		self.nodes_set = set([])
+    def __init__(self, core):
+        self.graph = {core: []}
+        self.images_nodes = {}
+        self.images_status = {}
+        self.status_choices = {'invalid': 'invalid', 'valid': 'valid', 'granularity_staged': 'granularity_staged', 'coverage_staged': 'coverage_staged'}
+        self.nodes_set = set([])
 
-	def add_nodes(self, nodes_to_add):
-		for (child, parent) in nodes_to_add:
-			self.graph.setdefault(parent, []).append(child)
-			self.nodes_set.add(child)
-		self.update_status(nodes_to_add=nodes_to_add, extract=None)
+    def add_nodes(self, nodes_to_add):
+        for (child, parent) in nodes_to_add:
+            self.graph.setdefault(parent, []).append(child)
+            self.nodes_set.add(child)
+        self.update_status(nodes_to_add=nodes_to_add, extract=None)
 
-	def add_extract(self, extract):
-		for (image_id, labels) in extract.items():
-			self.images_nodes.setdefault(image_id, []).append(labels)
-		self.update_status(extract=extract, nodes_to_add=None)
+    def add_extract(self, extract):
+        for (image_id, labels) in extract.items():
+            self.images_nodes.setdefault(image_id, []).append(labels)
+        self.update_status(extract=extract, nodes_to_add=None)
 
-	def get_extract_status(self):
-		return self.images_status
+    def get_extract_status(self):
+        return self.images_status
 
-	def update_status(self, extract, nodes_to_add):
-		if extract is not None:
-			for (image_id, labels) in extract.items():
-				self.images_status.setdefault(image_id, self.status_choices['valid'])
-				for label in labels:
-					if not (label in list(self.nodes_set)):
-						self.images_status[image_id] = self.status_choices['invalid']
-						break
+    def update_status(self, extract, nodes_to_add):
+        if extract is not None:
+            for (image_id, labels) in extract.items():
+                self.images_status.setdefault(image_id, self.status_choices['valid'])
+                for label in labels:
+                    if not (label in list(self.nodes_set)):
+                        self.images_status[image_id] = self.status_choices['invalid']
+                        break
 
 		# if nodes_to_add is not None:
 
